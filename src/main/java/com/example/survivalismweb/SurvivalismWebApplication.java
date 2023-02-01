55c5161505ab3f7d1f2426a5f65341578686e973
응애 <oystergw@naver.com>
2023-02-02 05:04:05 +0900

    Initial commit

---
+++ src/main/java/com/example/survivalismweb/SurvivalismWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.survivalismweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivalismWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurvivalismWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/survivalismweb/SurvivalismWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.survivalismweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurvivalismWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
